18501185bc0a951af8a3f8639777f7b4644e3275
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-08-24 10:39:45 +0530

    Add files via upload

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Exercise1
+{
+    public static void Main()
+    {
+        System.Console.Write("Hello, ");  // print in the same line
+        System.Console.WriteLine("Mainak Chaudhuri!");  // print and go to next line
+        System.Console.WriteLine("Welcome to C#");
+    }
+}

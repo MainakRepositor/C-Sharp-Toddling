4ff045d29c5f1c95fc81920b7c15c854829602b1
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-08-24 10:43:02 +0530

    Delete Exercise 1 directory

--- Exercise 1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Exercise1
-{
-    public static void Main()
-    {
-        System.Console.Write("Hello, ");  // print in the same line
-        System.Console.WriteLine("Mainak Chaudhuri!");  // print and go to next line
-        System.Console.WriteLine("Welcome to C#");
-    }
-}

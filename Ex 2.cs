65ad62aed866ddbdc5ab094c3cd5dd5f8dadd867
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-08-24 10:44:52 +0530

    Create Ex 2.cs

---
+++ Ex 2.cs
@@ -0,0 +1,9 @@
+public class Exercise1
+{
+    public static void Main( )
+    {
+         System.Console.Write("Hello");  // print in the same line
+         System.Console.WriteLine("Mainak Chaudhuri!"); // print in the next line
+         System.Console.WriteLine("Welcome to C#");
+    }
+}
